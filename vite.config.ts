a6ea81ca6239e80a2b0940f37730cb2ea558f3da
norhan1995 <norhan_rifaie@hotmail.com>
2026-09-25 22:53:27 +0300

    Fix production build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: { sourcemap: false },
-  test: { environment: 'node' }
+  build: { sourcemap: false }
 });
